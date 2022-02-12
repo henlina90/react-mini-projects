c43f10ce586ed88a0480ed4fd94ad47908247a96
henlina90 <henlina90>
2022-02-11 22:52:56 -0800

    init commit exercise

---
+++ react-static-site/README.md
@@ -0,0 +1,5 @@
+# React Facts Site
+
+A static site about React. This mini project was built as an intro to the basic principles of the library.
+
+https://scrimba.com/learn/learnreact
---
+++ react-static-site/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav className="nav">
+        <img src="/logo512.png" alt="react" />
+        <h1>ReactFacts</h1>
+        <p>React Course - Project 1</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
